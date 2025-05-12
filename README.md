0ab4d2b6a05b636c39181b297de4fe8ccbc8f0bb
Jacob <jacobulysseslockwood@gmail.com>
2025-05-12 10:50:14 -0400

    UI overhaul

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This site is intended to demonstrate what an APL-family language might look and feel like if re-imagined with fixed arity functions all the way down. It's not supposed to be a complete or production-ready language, but I want it to have enough features that one can use it and understand how its concepts might fit into a larger program.
 
-This is still in development; currently, most of the base language works and a few primitives, but there's still work that needs to be done. Notably incomplete base-language features are that bindings have not been implemented yet, and non-niladic expressions with a value as their rightmost tine currently error.
+This is still in development; currently, most of the base language works and a few primitives, but there's still work that needs to be done. I intend to implement probably twenty more primitives.
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import logo from "/APL_FIX.svg?url";
-import { createSignal, Index, ParentComponent, type Component } from "solid-js";
+import {
+  createSignal,
+  For,
+  ParentComponent,
+  Show,
+  type Component,
+} from "solid-js";
 import { lex, Parser, Visitor, type Token } from "./lang";
 import { dataBySymbol, display } from "./primitives";
 
@@ -17,7 +22,7 @@ const Highlight: Component<{ tokens: readonly Token[] }> = (props) => {
         const [name, glyph] = dataBySymbol(image);
         const color = {
           "monadic function": "text-lime-400",
-          "dyadic function": "text-sky-400",
+          "dyadic function": "text-emerald-400",
           "monadic modifier": "text-yellow-400",
           "dyadic modifier": "text-purple-300",
         }[glyph.kind];
@@ -33,7 +38,7 @@ const Highlight: Component<{ tokens: readonly Token[] }> = (props) => {
           </span>
         );
       case "string":
-        return <span class="text-cyan-300">{image}</span>;
+        return <span class="text-green-300">{image}</span>;
       case "number":
         return <span class="text-orange-400">{image}</span>;
       default:
@@ -43,13 +48,101 @@ const Highlight: Component<{ tokens: readonly Token[] }> = (props) => {
 };
 
 const Kbd: ParentComponent = (props) => (
-  <kbd class="bg-blue-900 px-1 font-[inherit] border-b-4 rounded-sm border-blue-700">
+  <kbd class="bg-green-900 px-1 font-[inherit] border-b-4 rounded-sm border-green-700">
     {props.children}
   </kbd>
 );
 
 export default function App() {
-  const [results, setResults] = createSignal<Result[]>([]);
+  return (
+    <div
+      class="p-5 sm:p-10 lg:py-20 text-emerald-300 selection:bg-green-800 flex flex-col
+             mx-auto md:w-3/4 min-h-screen bg-emerald-950/50"
+    >
+      <div class="lg:flex gap-10 ">
+        <div class="lg:w-2/5 max-w-prose mb-10">
+          <img
+            class="w-30 h-30 mx-auto"
+            src="/APL_FIX.svg"
+            alt="FIX APL logo."
+          />
+          <h1 class="text-2xl text-center text-emerald-200">Fixed-arity APL</h1>
+          <p class="italic text-center mb-10">
+            A simple APL derivative, built on fixed-arity functions
+          </p>
+          <details>
+            <summary class="text-emerald-500 underline underline-offset-2">
+              Why fixed arity
+            </summary>
+            <p class="mt-1">
+              I've written about the motivations for fixed-arity to a greater
+              extent{" "}
+              <a href="https://example.com" class="text-green-500 underline">
+                here
+              </a>
+              , but to summarize the main points:
+            </p>
+            <ul class="list-disc pl-5">
+              <li>
+                <p>
+                  The overloading of glyphs to have different meanings when
+                  called monadically versus dyadically can be confusing. This is
+                  not universally true, and many glyph-pairings just feel
+                  natural, but others don't. Marshall Lochbaum has written about
+                  this{" "}
+                  <a
+                    href="https://mlochbaum.github.io/BQN/commentary/overload.html"
+                    class="text-green-500 underline"
+                  >
+                    here
+                  </a>{" "}
+                  in the context of BQN.
+                </p>
+              </li>
+              <li>
+                <p>
+                  In Uiua, functions having fixed arity lets modifiers have
+                  different behavior depending on the arities of their operands.
+                </p>
+              </li>
+            </ul>
+          </details>
+          <details>
+            <summary class="text-emerald-500 underline underline-offset-2">
+              How to use this page
+            </summary>
+            <p class="text-emerald-300 mt-1">
+              Click in the REPL box to enter statements. Glyphs can be entered
+              by typing in the appropriate alias given in the documentation. Use{" "}
+              <Kbd>Shift+Enter</Kbd> to enter a newline instead of entering the
+              code. Click on a previously entered segment to paste it into the
+              textbox.
+            </p>
+          </details>
+        </div>
+        <main class="flex-grow max-w-[80ch] mx-auto">
+          <Repl />
+        </main>
+      </div>
+      <footer class="text-center mt-60 text-emerald-600">
+        Created by{" "}
+        <a href="https://github.com/Jacob-Lockwood" class="underline">
+          Jacob Lockwood
+        </a>
+        .
+      </footer>
+    </div>
+  );
+}
+
+function Repl() {
+  const [results, setResults] = createSignal<Result[]>([
+    /* {
+    // source: `"Hello"`
+  } */
+  ]);
+  const [settingsOpen, setSettingsOpen] = createSignal(false);
+
   const visitor = new Visitor();
   const process = (source: string) => {
     let tokens: Token[] | null = null;
@@ -65,103 +158,98 @@ export default function App() {
       error = e + "";
       console.error(e);
     }
-    setResults((results) => [...results, { source, tokens, output, error }]);
+    setResults((results) => [{ source, tokens, output, error }, ...results]);
   };
   let textarea!: HTMLTextAreaElement;
   return (
     <div
-      class="h-screen p-10 lg:p-20 bg-sky-950 text-sky-200 selection:bg-blue-800
-             font-[Uiua386,TinyAPL386]"
+      class="h-max overflow-y-scroll bg-black/20 font-mono p-4 pt-1 flex
+                   flex-col gap-2 rounded-md selection:bg-green-950"
     >
-      <div class="lg:flex gap-10 max-w-6xl mx-auto">
-        <div class="flex-grow max-w-prose lg:w-md mb-10">
-          <img class="w-30 mx-auto" src={logo} />
-          <h1 class="text-2xl text-center">Fixed-arity APL Demo</h1>
-          <p class="italic text-center mb-10">
-            APL, reimagined with fixed-arity function
-          </p>
-          <details>
-            <summary class="text-sky-500 underline underline-offset-2">
-              How to use this demo
-            </summary>
-            <p class="text-sky-300 mt-1">
-              Click in the REPL box to enter statements. Glyphs can be entered
-              by typing in the appropriate alias given in the documentation. Use{" "}
-              <Kbd>Shift+Enter</Kbd> to enter a newline instead of entering the
-              code. Click on a previously entered segment to paste it into the
-              textbox.
-            </p>
-          </details>
+      <div class="flex justify-between items-center">
+        <h2 class="text-sm">REPL</h2>
+        <button
+          class="text-2xl cursor-pointer"
+          title="Configuration options"
+          onClick={() => setSettingsOpen((b) => !b)}
+        >
+          ⚙
+        </button>
+      </div>
+      <div class="h-80 flex flex-col">
+        <div
+          class="border-b-2 border-emerald-500 w-full p-4"
+          classList={{ hidden: !settingsOpen() }}
+        >
+          <p class="text-sm italic mb-1">Settings</p>
+          <div class="flex gap-4">
+            <label for="clear">Clear prompt on enter</label>
+            <input type="checkbox" name="clear" id="clear" />
+          </div>
         </div>
-        <main class="w-full max-w-[80ch]">
-          <div
-            class="h-max overflow-y-scroll bg-sky-900 font-mono p-4 pt-1 flex
-                   flex-col gap-2 rounded-md selection:bg-blue-950"
-          >
-            <h2 class="text-sm text-sky-500">REPL</h2>
-            <Index each={results()}>
-              {(result) => (
-                <>
-                  <pre
-                    class="pl-[8ch] hover:bg-blue-950/20"
-                    onClick={(e) =>
-                      (textarea.value ||= e.currentTarget.textContent ?? "")
-                    }
-                  >
-                    <code>
-                      {result().tokens ? (
-                        <Highlight tokens={result().tokens!} />
-                      ) : (
-                        result().source
-                      )}
-                    </code>
-                  </pre>
-                  {result().output ? (
-                    <pre class="text-blue-300">{result().output}</pre>
-                  ) : (
-                    <pre class="text-red-300">{result().error}</pre>
-                  )}
-                </>
-              )}
-            </Index>
-            <div class="grid" id="wrapper">
-              <textarea
-                id="code-input"
-                ref={textarea}
-                class="w-full rounded-sm ring-blue-500
+        <ul class="flex flex-col-reverse h-full w-full overflow-scroll">
+          <For each={results()}>
+            {(result) => (
+              <li>
+                <pre
+                  class="pl-[8ch] hover:bg-teal-950/50"
+                  onClick={(e) =>
+                    (textarea.value ||= e.currentTarget.textContent ?? "")
+                  }
+                >
+                  <code>
+                    {result.tokens ? (
+                      <Highlight tokens={result.tokens!} />
+                    ) : (
+                      result.source
+                    )}
+                  </code>
+                </pre>
+                {result.output ? (
+                  <pre class="text-green-300">{result.output}</pre>
+                ) : (
+                  <pre class="text-red-300">{result.error}</pre>
+                )}
+              </li>
+            )}
+          </For>
+        </ul>
+      </div>
+      <div class="grid overflow-x-scroll p-2 -m-2 mt-auto" id="wrapper">
+        <textarea
+          id="code-input"
+          ref={textarea}
+          aria-label="REPL input line"
+          class="w-full rounded-sm ring-green-500
                      focus:outline-0 ring-1 focus:ring-2 
                      resize-none overflow-hidden"
-                rows="1"
-                onKeyDown={(ev) => {
-                  if (ev.key === "Enter" && !ev.shiftKey) {
-                    ev.preventDefault();
-                    process(textarea.value);
-                    // todo: make clearing the textarea a configurable option
-                    // textarea.value = "";
-                  }
-                }}
-                onInput={() =>
-                  (textarea.parentElement!.dataset.value = textarea.value)
-                }
-              ></textarea>
-            </div>
-            <style>{`
-            #wrapper::after {
-              content: attr(data-value) " ";
-              white-space: pre-wrap;
-              visibility: hidden;
+          rows="1"
+          onKeyDown={(ev) => {
+            if (ev.key === "Enter" && !ev.shiftKey) {
+              ev.preventDefault();
+              process(textarea.value);
+              // todo: make clearing the textarea a configurable option
+              // textarea.value = "";
             }
-            #code-input, #wrapper::after {
-              padding: 1px;
-              padding-left: 8ch;
-              font: inherit;
-              grid-area: 1 / 1 / 2 / 2;
-            }
-          `}</style>
-          </div>
-        </main>
+          }}
+          onInput={() =>
+            (textarea.parentElement!.dataset.value = textarea.value)
+          }
+        ></textarea>
       </div>
-      <footer></footer>
+      <style>{`
+        #wrapper::after {
+          content: attr(data-value) " ";
+          white-space: pre-wrap;
+          visibility: hidden;
+        }
+        #code-input, #wrapper::after {
+          padding: 1px;
+          padding-left: 8ch;
+          font: inherit;
+          grid-area: 1 / 1 / 2 / 2;
+        }
+      `}</style>
     </div>
   );
 }
